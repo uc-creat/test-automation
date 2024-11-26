f5c3c306ef6addda10c9054c0719a4136a2043fb
uc-creat <utkarshchauhanutc26@gmail.com>
2024-11-26 15:25:43 +0530

    able to read txt file and store uuid

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from findDuplicate import duplicateOne,duplicateTwo
 from flask import Flask
+import sys
 
 
 from flask import Flask, request, render_template
@@ -15,8 +16,11 @@ def index():
 
 @app.route('/read-form', methods=['POST'])
 def read_form():
-	data = request.form
-	finalScore = duplicateTwo(data)
-	return str(finalScore)
-
+  data = request.form
+  finalScore = duplicateTwo(data)
+  original_stdout = finalScore
+  with open('../text.txt','w') as f:
+       sys.stdout = f
+       print(finalScore)
+  return str(finalScore)
 
